23c61629e66953262366d48e27d95817df0b7ad2
nooraaroon <aayaa@live.com>
2015-11-06 12:22:22 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,37 @@
+var xmlHttp=createXmlHttpRequestObject(); 
 
+function createXmlHttpRequestObject()
+{
+  var xmlHttp; 
+  
+  if(window.ActiveXObject)
+  {
+    try
+    {
+      xmlHttp=new ActiveXObject("Microsoft.XMLHTTP"); 
+    }
+    catch(e)
+    {
+      xmlHttp=false; 
+    }
+    else
+    {
+      try
+      {
+        xmlHttp=new XMLHttpRequest(); 
+      }
+      catch(e)
+      {
+        xmlHttp=false; 
+      }
+    }
+    if(!xmlHttp)
+    {
+      alert("Unable to create object");
+    }
+    else
+    {
+      return xmlHttp; 
+    }
+  }
+}
